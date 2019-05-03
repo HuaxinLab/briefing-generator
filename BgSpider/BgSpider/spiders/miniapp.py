57fe8ffc9fa21771490d38986f3d3f8bba15293a
acusp <acusp.xu@gmail.com>
2019-05-03 23:58:23 +0800

    Fix some bugs

--- BgSpider/BgSpider/spiders/miniapp.py
+++ BgSpider/BgSpider/spiders/miniapp.py
@@ -1,36 +1,38 @@
 # -*- coding: utf-8 -*-
 import scrapy
-import datetime
+from datetime import datetime, date, timedelta
 from BgSpider.items import BgspiderItem
 
 
 class MiniappSpider(scrapy.Spider):
     name = 'miniapp'
     # allowed_domains = ['minapp.com/miniapp']
+    yesterday = (date.today() + timedelta(days = -1)).strftime("%Y-%m-%d") 
     base_url = 'https://minapp.com'
     start_urls = ['https://minapp.com/miniapp/']
 
     def parse(self, response):
         appList = response.css('.main-content--specify li')
         for app in appList:
-            if 'page' not in str(app):
+            if 'page' not in str(app): 
                 item = BgspiderItem(post_type='miniapp') 
                 item['post_abstract'] = app.css('span::text').get()
                 item['post_date'] = self.dateFormat(app.css('time::text').get())
                 item['post_title'] = app.css('h2::text').get()
                 item['post_url'] = self.base_url + app.css('a::attr(href)').get()
-                yield item
+                if self.yesterday == item['post_date']:
+                    yield item
 
     def dateFormat(self, date_str):
         # in = '星期二, 30 四月 2019 15:21:46 +0800'
         # out = 'Thursday, 30 April 2019 15:21:46 +0800'
-        month = {'January':'一月 ','February':'二月 ', 'March':'三月', 
-                'April':'四月',  'May':'五月', 'June':'六月', 
-                'July':'七月', 'August':'八月 ', 'September':'九月', 
-                'October':'十月', 'November':'十一月', 'December':'十二月'}
+        month = {'Jan':'一月', 'Feb':'二月', 'Mar':'三月', 
+                 'Apr':'四月', 'May':'五月', 'Jun':'六月', 
+                 'Jul':'七月', 'Aug':'八月', 'Sep':'九月', 
+                 'Oct':'十月', 'Nov':'十一月', 'Dec':'十二月'}
 
-        week = {'Monday':'星期一', 'Tuesday':'星期二', 'Wednesday':'星期三',
-                'Thursday':'星期四', 'Friday':'星期五', 'Saturday':'星期六', 'Sunday':'星期日'}
+        week = {'Mon':'星期一', 'Tue':'星期二', 'Wed':'星期三',
+                'Thu':'星期四', 'Fri':'星期五', 'Sat':'星期六', 'Sun':'星期日'}
 
         for item in month.items():
             if item[1] in date_str:
@@ -39,5 +41,6 @@ class MiniappSpider(scrapy.Spider):
         for item in week.items():
             if item[1] in date_str:
                 date_str = date_str.replace(item[1], item[0])
-        
-        return datetime.datetime.strptime(date_str, "%A, %d %B %Y %H:%M:%S +0800").strftime("%Y-%m-%d")
+
+        long_date = datetime.strptime(date_str, "%a, %d %b %Y %H:%M:%S +0800")
+        return long_date.strftime("%Y-%m-%d")
--- BgSpider/BgSpider/spiders/product_next.py
+++ BgSpider/BgSpider/spiders/product_next.py
@@ -7,8 +7,8 @@ from datetime import datetime, date, timedelta
 class ProductNextSpider(scrapy.Spider):
     name = 'product_next'
     #allowed_domains = ['next.36kr.com']
-    post_date = (date.today() + timedelta(days = -1)).strftime("%Y-%m-%d") 
-    start_urls = ['http://next.36kr.com/posts?start_on=' + post_date]
+    yesterday = (date.today() + timedelta(days = -1)).strftime("%Y-%m-%d") 
+    start_urls = ['http://next.36kr.com/posts?start_on=' + yesterday]
 
     def parse(self, response):
         section = response.css('section')[0]
@@ -16,8 +16,9 @@ class ProductNextSpider(scrapy.Spider):
         for pdt in productList:
             item = BgspiderItem(post_type='product') 
             item['post_abstract'] = pdt.css('.post-tagline::text').get()
-            item['post_date'] = self.post_date
+            item['post_date'] = self.yesterday
             item['post_title'] = pdt.css('.post-url::text').get()
             # css('.post-url').re(r'title=\"(.*?)\"')
-            item['post_url'] = pdt.css('.post-url').attrib['title']
-            yield item
+            item['post_url'] = 'http://' + pdt.css('.post-url').attrib['title']
+            if 'minapp.com' not in item['post_url']:
+                yield item
