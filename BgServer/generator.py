67150ce0b0c38270cc6645fa6a80383afd75a875
acusp <acusp.xu@gmail.com>
2019-05-11 00:00:06 +0800

    refactor: delete resultHandler

--- BgServer/generator.py
+++ BgServer/generator.py
@@ -56,34 +56,6 @@ class IndexHandler(tornado.web.RequestHandler):
         self.render("index.html", headline=headline, post_date=post_date, 
             post_type=post_type_zh, postList=postList)
     
-# class BriefingHandler(tornado.web.RequestHandler):
-#     typeMap = {'product': '产品', 'miniapp': '小程序'}
-
-#     def fetch_data(self, post_type, post_date):
-#         client = MongoClient()
-#         database = client['BgSpider']
-#         collection = database[post_type+'_items']
-#         postList = collection.find(
-#             {'post_date':post_date}, 
-#             {'_id': 0, 'post_abstract': 1, 'post_title': 1, 'post_url': 1}
-#         )
-#         client.close()
-#         return postList
-
-#     def post(self):
-#         headline = self.get_argument('headline')
-#         post_type = self.get_arguments('post_type')
-#         post_date = self.get_argument('post_date')
-#         postList = []
-#         post_type_zh = []
-
-#         for i in range(len(post_type)):
-#             postList.append(self.fetch_data(post_type[i], post_date))
-#             post_type_zh.append(self.typeMap[post_type[i]])
-
-#         self.render('result.html', headline=headline, post_date=post_date, 
-#             post_type=post_type_zh, postList=postList)
-
 class PosesModule(tornado.web.UIModule):
     def render(self, posts):
         return self.render_string("modules/posts.html", posts=posts)
